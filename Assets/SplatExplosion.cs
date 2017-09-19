9befed4b3b1b697cb7bcfd0b0af81ef98afc78cb
GameDev <arcademissile@gmail.com>
2017-09-19 14:38:13 -0500

    Splat expl looks better

--- Assets/SplatExplosion.cs
+++ Assets/SplatExplosion.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class SplatExplosion : MonoBehaviour {
 	Material mat;
 	float cutoff = 0;
-	float scale = 1;
+	float scale = 0.1f;
 
 	void Start () {
-		mat = GetComponent<Renderer> ().material;	
+		mat = GetComponent<Renderer> ().material;
 	}
 	
 	void Update () {
 		cutoff += 1.9f * Time.deltaTime;
 		mat.SetFloat ("_SplatCutoff", cutoff);
-		scale += 3.0f * Time.deltaTime;
+		scale += 6.0f * Time.deltaTime;
 		transform.localScale = Vector3.one * scale;
 	}
 }
