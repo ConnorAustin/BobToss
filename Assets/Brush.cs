eca12a7c29dbbd0f7122ec8e8c8423211010c495
GameDev <arcademissile@gmail.com>
2017-09-21 10:31:49 -0500

    Added brush

---
+++ Assets/Brush.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brush : MonoBehaviour {
+	public int boneCount;
+	public Transform target;
+	public float maxAngle;
+
+	Transform[] bones;
+	Transform tip;
+
+	void Start () {
+		bones = new Transform[boneCount];
+		string bonePath = "Armature/";
+
+		for (int i = 0; i < boneCount; ++i) {
+			bonePath += "Bone" + i;
+			bones [i] = transform.Find (bonePath);
+			bonePath += "/";
+		}
+		tip = transform.Find(bonePath + "Tip");
+	}
+
+	float IKReward() {
+		float dist = Mathf.Max(0.01f, Vector3.Distance (tip.position, target.position));
+		return 1.0f / dist;
+	}
+
+	void IK(int boneIndex) {
+		if (boneIndex >= boneCount)
+			return;
+
+		Vector3 bendDirection = Vector3.right;
+
+		float step = 3.0f;
+		float decay = 0.8f;
+		const int iterations = 50;
+		Transform bone = bones [boneIndex];
+
+		float finalOrientation = 0;
+		for (int i = 0; i < iterations; i++) {
+			float curReward = IKReward ();
+
+			// Option A
+			bone.Rotate (bendDirection * step);
+			float optionAReward = IKReward ();
+			IKReset (boneIndex);
+			bone.Rotate (bendDirection * finalOrientation);
+
+			// Option B
+			bone.Rotate (bendDirection * -step);
+			float optionBReward = IKReward ();
+			IKReset (boneIndex);
+			bone.Rotate (bendDirection * finalOrientation);
+
+			if (optionAReward > curReward && optionAReward > optionBReward) {
+				finalOrientation += step;
+			} else if (optionBReward > curReward && optionBReward > optionAReward) {
+				finalOrientation += -step;
+			}
+
+			step *= decay;
+		}
+		IKReset (boneIndex);
+		finalOrientation = Mathf.Clamp (finalOrientation, -maxAngle, maxAngle);
+		bone.Rotate (bendDirection * finalOrientation);
+		IK (boneIndex + 1);
+	}
+
+	void IKReset() {
+		for (int i = 0; i < boneCount; i++) {
+			IKReset (i);
+		}
+	}
+
+	void IKReset(int boneIndex) {
+		bones[boneIndex].transform.localRotation = Quaternion.Euler (Vector3.zero);
+	}
+	
+	void Update () {
+		IKReset ();
+		IK (0);
+	}
+}
