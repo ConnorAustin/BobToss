e1316e9cdc96c13b33e96b58f8ca3c59598ce5e0
GameDev <arcademissile@gmail.com>
2017-10-03 15:53:41 -0500

    Worked on bar

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager manager;
+
+	[HideInInspector]
+	public float coverage;
+
+	public float desiredCoverage;
+	SplatCanvas[] canvases;
+
+	void Start () {
+		manager = this;
+		var building = GameObject.Find ("Building");
+		canvases = building.GetComponentsInChildren<SplatCanvas> ();
+	}
+	
+	void Update () {
+		foreach(var sc in canvases) {
+			coverage += sc.coverage;
+		}
+		coverage /= (float)canvases.Length;
+		coverage /= desiredCoverage;
+		coverage = Mathf.Min (1, coverage);
+	}
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+	public RectTransform backBar;
+	public RectTransform bar;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		float coverage = GameManager.manager.coverage;
+		float barWidth = Mathf.Lerp (0, backBar.sizeDelta.x, coverage);
+		bar.sizeDelta = new Vector2 (barWidth, backBar.sizeDelta.y);
+	}
+}
